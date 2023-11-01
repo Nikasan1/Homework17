af28cfec6cbb50740361688829cdb39dad44e41c
Nikasan1 <nikasan4@gmail.com>
2023-11-01 19:04:49 +0100

    Second commit

--- src/Bank.java
+++ src/Bank.java
@@ -6,21 +6,27 @@ public class Bank {
     public Bank() {
         Random rnd = new Random();
         double maxExchangeRate = 0;
-        int temp = 0;
         String[] bankName = new String[]{"ПРИВАТБАНК", "Сенс Банк", "Сітібанк", "ІНГ Банк Україна", "Ощадбанк", "Укрексімбанк", "УКРГАЗБАНК", "Райффайзен Банк", "УКРСИББАНК", "ПУМБ", "КРЕДІ АГРІКОЛЬ БАНК", "Південний", "КРЕДОБАНК", "Банк Кредит Дніпро"};
         double[] exchangeRate = new double[14];
+        int[] temp = new int[exchangeRate.length];
         for (int i = 0; i < 14; i++) {
             exchangeRate[i] = rnd.nextDouble() + rnd.nextInt(5) + 35;
         }
+        int j=0;
         for (int i = 0; i < 14; i++) {
             if (i == 0) {
                 maxExchangeRate = exchangeRate[0];
             } else if (exchangeRate[i] > maxExchangeRate) {
                 maxExchangeRate = exchangeRate[i];
-                temp = i;
+                temp[0] = i;
+            }else if (exchangeRate[i]==maxExchangeRate) {
+                j++;
+                temp[j]=i;
             }
         }
-        System.out.println("Найвигідніщий курс було знайдено в банку : " + bankName[temp] + ", за курсом: " + maxExchangeRate + "грн за 1$.");
+        for (int i=0;i<j+1;i++) {
+            System.out.println("Найвигідніщий курс було знайдено в банку : " + bankName[temp[i]] + ", за курсом: " + maxExchangeRate + "грн за 1$.");
+        }
         /*System.out.println(Arrays.toString(exchangeRate)); Контрольні стрічки
         System.out.println(Arrays.toString(bankName));*/
     }
